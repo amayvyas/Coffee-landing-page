1eb5ead69b9aa8b8eb09fc5c5b4033c5796538c9
22AAVS <22amayvyas@gmail.com>
2024-03-01 17:29:34 +0530

    Dummydata is added

---
+++ JS/my-vite-app/dummydata.js
@@ -0,0 +1,17 @@
+const dummyData = [
+    { id: 1, name: 'Coffe Latte', description: 'Description 1' },
+    { id: 2, name: 'Capecuino', description: 'Description 2' },
+    { id: 3, name: 'Mocha', description: 'Description 3' },
+    { id: 4, name: 'Iced', description: 'Description 2' },
+    { id: 5, name: 'Filter Coffe', description: 'Description 2' },
+    { id: 6, name: 'Hazelnut Coffe', description: 'Description 2' },
+    { id: 7, name: 'Rum coffe', description: 'Description 2' },
+    { id: 8, name: 'Coffe granda', description: 'Description 2' },
+    { id: 9, name: 'Irish', description: 'Description 2' },
+    { id: 10, name: 'Malta', description: 'Description 2' },
+    { id: 11, name: 'Chicory Mix', description: 'Description 2' },
+    { id: 12, name: 'Fusion', description: 'Description 2' }
+    
+  ];
+  
+  export default dummyData;
--- JS/my-vite-app/src/components/items/Items.jsx
+++ JS/my-vite-app/src/components/items/Items.jsx
@@ -2,166 +2,83 @@ import "../items/items.css"
 import { Carousel, Card, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import dummyData from "../../../dummydata";
 
 
 export default function Items() {
+
+
+  const chunkArray = (arr, size) => {
+    return Array.from({ length: Math.ceil(arr.length / size) }, (_, index) => {
+      const start = index * size;
+      return arr.slice(start, start + size);
+    });
+  };
+
+  // Chunk the dummy data array into groups of four items
+  const chunkedData = chunkArray(dummyData, 4);
   return (
-  <div className="main-container">
-      <Carousel indicators = {false}
-        prevIcon={<BsArrowLeft className="custom-carousel-control" />}
-        nextIcon={<BsArrowRight className="custom-carousel-control" />}>
-        <Carousel.Item>
-          <span className="coffee">OUR SPECIAL COFFEE</span>
-
-         <Row className="row">
-            <Col>
-             <Card className="custom-card" style={{ width: '15rem' }}>
-                <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-                <Card.Body className="card-body">
-                 <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                 <Card.Text>
-                  Our Coffee tastes best in the town.
-                 </Card.Text>
-                 <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-               </Card.Body>
-            </Card>
-          </Col>
-
-          <Col>
-            <Card className="custom-card" style={{ width: '15rem' }}>
-              <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-              <Card.Body>
-              <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                <Card.Text>
-                Our Coffee tastes best in the town.
-                </Card.Text>
-                <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          
-          <Col>
-            <Card className="custom-card" style={{ width: '15rem' }}>
-              <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-              <Card.Body>
-              <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                <Card.Text>
-                Our Coffee tastes best in the town.
-                </Card.Text>
-                <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-
-          <Col>
-            <Card className="custom-card" style={{ width: '15rem' }}>
-              <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-              <Card.Body>
-              <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                <Card.Text>
-                Our Coffee tastes best in the town.
-                </Card.Text>
-                <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-
-        </Row>
-      </Carousel.Item>
-      {/* Add additional Carousel.Items for more slides */}
-    </Carousel>
+    
+<>
+<Carousel indicators={false} prevIcon={<BsArrowLeft className="custom-carousel-control" />} nextIcon={<BsArrowRight className="custom-carousel-control" />}>
+        {chunkedData.map((chunk, index) => (
+          <Carousel.Item key={index}>
+            <span className="coffee">OUR SPECIAL COFFEE</span>
+            <Row className="row">
+              {chunk.map((item, itemIndex) => (
+                <Col key={itemIndex}>
+                  <Card className="custom-card" style={{ width: '15rem' }}>
+                    <Card.Img variant="top" src="../src/assets/coffee.jpg" />
+                    <Card.Body className="card-body">
+                      <Card.Title className="cardTitle">{item.name}</Card.Title>
+                      <Card.Text>
+                        {item.description}
+                      </Card.Text>
+                      <div className="priceMain">
+                        <div className="price">Rs.150</div>
+                        <button className="ordernow">Order Now</button>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
 
-    <Carousel indicators = {false}
-        prevIcon={<BsArrowLeft className="custom-carousel-control" />}
-        nextIcon={<BsArrowRight className="custom-carousel-control" />}>
-        <Carousel.Item>
-          <span className="coffee">OUR SPECIAL COFFEE</span>
-
-         <Row className="row1">
-            <Col>
-             <Card className="custom-card" style={{ width: '15rem' }}>
-                <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-                <Card.Body className="card-body">
-                <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                 <Card.Text>
-                 Our Coffee tastes best in the town.
-                 </Card.Text>
-                 <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-               </Card.Body>
-            </Card>
-          </Col>
-
-          <Col>
-            <Card className="custom-card" style={{ width: '15rem' }}>
-              <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-              <Card.Body>
-              <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                <Card.Text>
-                Our Coffee tastes best in the town.
-                </Card.Text>
-                <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          
-          <Col>
-            <Card className="custom-card" style={{ width: '15rem' }}>
-              <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-              <Card.Body>
-              <Card.Title className="cardTitle">Lungo Coffee</Card.Title>
-                <Card.Text>
-                Our Coffee tastes best in the town.
-                </Card.Text>
-                <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-
-          <Col>
-            <Card className="custom-card" style={{ width: '15rem' }}>
-              <Card.Img variant="top" src="../src/assets/coffee.jpg" />
-              <Card.Body>
-              <Card.Title>Lungo Coffee</Card.Title>
-                <Card.Text>
-                Our Coffee tastes best in the town.
-                </Card.Text>
-                <div className="priceMain">
-                 <div className="price">Rs.150</div>
-                 <button className="ordernow">Order Now</button>
-                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-
-        </Row>
-      </Carousel.Item>
-      {/* Add additional Carousel.Items for more slides */}
-    </Carousel>
+      <Carousel indicators={false} prevIcon={<BsArrowLeft className="custom-carousel-control" />} nextIcon={<BsArrowRight className="custom-carousel-control" />}>
+        {chunkedData.map((chunk, index) => (
+          <Carousel.Item key={index}>
+            <span className="coffee">OUR SPECIAL COFFEE</span>
+            <Row className="row">
+              {chunk.map((item, itemIndex) => (
+                <Col key={itemIndex}>
+                  <Card className="custom-card" style={{ width: '15rem' }}>
+                    <Card.Img variant="top" src="../src/assets/coffee.jpg" />
+                    <Card.Body className="card-body">
+                      <Card.Title className="cardTitle">{item.name}</Card.Title>
+                      <Card.Text>
+                        {item.description}
+                      </Card.Text>
+                      <div className="priceMain">
+                        <div className="price">Rs.150</div>
+                        <button className="ordernow">Order Now</button>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+</>
+  
+    
 
 
-  </div>
    
   );
 }
