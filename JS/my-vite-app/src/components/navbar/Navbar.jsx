f7e9a10531f0b679cdc449dd579c2ba60c032307
amay2520 <22amayvyas@gmail.com>
2024-06-28 11:40:29 +0530

    trial

--- JS/my-vite-app/src/components/navbar/Navbar.jsx
+++ JS/my-vite-app/src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <section className="mainContainer">
     <Header/>
     <div className="textArea">
-      <div className="welcome">Welcome!</div>
+      <div className="welcome">Welcome to aamy's</div>
       <div className="text">We serve the richest coffee</div>
       <div className="text">in the city!</div>
       <div className="button">
